67cca5ed7aea049dbee18be671c2bb2dfae94957
atsushi130 <atsushi_main@outlook.jp>
2018-09-09 19:46:04 +0900

    add: project files

---
+++ CoordinatorKit/CoordinatorKit.h
@@ -0,0 +1,19 @@
+//
+//  CoordinatorKit.h
+//  CoordinatorKit
+//
+//  Created by Atsushi Miyake on 2018/09/09.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RoutableCoordinator.
+FOUNDATION_EXPORT double RoutableCoordinatorVersionNumber;
+
+//! Project version string for RoutableCoordinator.
+FOUNDATION_EXPORT const unsigned char RoutableCoordinatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RoutableCoordinator/PublicHeader.h>
+
+
